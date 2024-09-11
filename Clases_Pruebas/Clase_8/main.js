024df05e3c6eb707f4fb9b593d8333de8ce3d117
jesus.villegas <jesus.villegas@colombia.com>
2024-09-10 19:58:14 -0500

    primera entrega

---
+++ Clases_Pruebas/Clase_8/main.js
@@ -0,0 +1,22 @@
+// Actividad de clase #1
+
+for (let i = 1; i < 10; i++){
+    if(i == 1){
+        title = "BMW M5";
+        pricing = 500;
+        description = "xd";
+    }else if(i == 1){
+        title = "no se";
+        pricing = 4000;
+        description = "xd";
+    }else if(i == 1){
+        title = "GTA 5";
+        pricing = 50000;
+        description = "xd";
+    }
+    else{
+        title = "BMW M5";
+        pricing = 500;
+        description = "xd";
+    }
+}
