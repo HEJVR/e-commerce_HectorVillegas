58453c572745d2db16b163585baa0932ba2f0d25
jesus.villegas <jesus.villegas@colombia.com>
2024-09-17 17:50:48 -0500

    1er Sprint - Terminado

--- Clases_Pruebas/Clase_7/main.js
+++ Clases_Pruebas/Clase_7/main.js
@@ -1,22 +1,54 @@
 // Actividad de clase #1
 
-const numero1 = prompt("Ingresa el primer numero");
-const texto_tipo_de_signos = "+ : Sumar" + "\n" + "- : Resta" + "\n" + "* : Multiplicacion" + "\n" + "/ : Division" + "\n";
-const tipo_operacion = prompt(texto_tipo_de_signos);
-const numero2 = prompt("Ingresa el segundo numero");
+// const numero1 = prompt("Ingresa el primer numero");
+// const texto_tipo_de_signos = "+ : Sumar" + "\n" + "- : Resta" + "\n" + "* : Multiplicacion" + "\n" + "/ : Division" + "\n";
+// const tipo_operacion = prompt(texto_tipo_de_signos);
+// const numero2 = prompt("Ingresa el segundo numero");
 
-if (tipo_operacion == "+" || tipo_operacion == "-" || tipo_operacion == "*" || tipo_operacion == "/"){
-    if (tipo_operacion == "+"){
-        alert("La suma de los dos numeros es: " + (parseInt(numero1) + parseInt(numero2)));
-    }else if (tipo_operacion == "-"){
-        alert("La resta de los dos numeros es: " + (parseInt(numero1) - parseInt(numero2)));
-    }else if (tipo_operacion == "*"){
-        alert("La multiplicaion de los dos numeros es: " + (parseInt(numero1) * parseInt(numero2)));
-    }else if (tipo_operacion == "/"){
-        alert("La division de los dos numeros es: " + (parseInt(numero1) / parseInt(numero2)));
-    }
-}else{
-    alert("Error, tipo de signo invalida");
+// if (tipo_operacion == "+" || tipo_operacion == "-" || tipo_operacion == "*" || tipo_operacion == "/"){
+//     if (tipo_operacion == "+"){
+//         alert("La suma de los dos numeros es: " + (parseInt(numero1) + parseInt(numero2)));
+//     }else if (tipo_operacion == "-"){
+//         alert("La resta de los dos numeros es: " + (parseInt(numero1) - parseInt(numero2)));
+//     }else if (tipo_operacion == "*"){
+//         alert("La multiplicaion de los dos numeros es: " + (parseInt(numero1) * parseInt(numero2)));
+//     }else if (tipo_operacion == "/"){
+//         alert("La division de los dos numeros es: " + (parseInt(numero1) / parseInt(numero2)));
+//     }
+// }else{
+//     alert("Error, tipo de signo invalida");
+// }
+
+
+
+
+
+
+
+
+
+// segundo ejercicio,
+const numero = 2;
+
+switch (numero) {
+    case 1:
+        console.log("Es 1");
+        break;
+
+    case 2:
+        console.log("Es 2");
+        break;
+
+    default:
+        console.log("No es 1 ni 2, es la opcion default");
+        break;
+}
+
+
+
+
+for(let i = 5; i < 10; i++) {
+    console.log(i);
 }
 
 
@@ -24,20 +56,28 @@ if (tipo_operacion == "+" || tipo_operacion == "-" || tipo_operacion == "*" || t
 
 
 
+let h1 = document.querySelector("h1");
 
+h1.innerText = "Productos";
 
+let array = [];
 
-// segundo ejercicio, notas
+for (let i = 1; i < 10; i++){
+    array.push(``);
+}
+
+console.log(array);
+
 
 // const nota = prompt("ingresa la nota que te sacaste");
 
-if(nota > 10){
+if (nota > 10) {
     console.log("Dios miooo, A+++");
-}else if (nota >= 7){
+} else if (nota >= 7) {
     console.log("Te sacaste una A");
-}else if(nota >= 4){
+} else if (nota >= 4) {
     console.log("Te sacaste una B");
-}else{
+} else {
     console.log("Te sacaste una C, que trite....");
 }
 
@@ -50,7 +90,7 @@ if(nota > 10){
 
 const feriado = false;
 
-if(feriado) {
+if (feriado) {
     console.log("esto funciona")
 }
 else {
@@ -60,29 +100,29 @@ else {
 
 
 
-if (2 + 2 == 3 + 1){
+if (2 + 2 == 3 + 1) {
     console.log("2+2 si es igual a 3+1");
 }
 
-if (2 > 1){
+if (2 > 1) {
     console.log("es mayor");
 }
 
-if (2 <= 10){
+if (2 <= 10) {
     console.log("10 es mayor o igual a 2");
 }
 
-if (2 >= 10){
+if (2 >= 10) {
     console.log("2 no es mayor o igual a 10");
 }
 
-if ("10" == 10){
+if ("10" == 10) {
     console.log("10 es igual a 10");
 }
 
-if ("10" === 10){
+if ("10" === 10) {
     console.log("10 es igual a 10");
-}else{
+} else {
     console.log("10 no es igual a 10, porque son diferente tipo de dato");
 }
 
---
+++ main_principal.js
@@ -0,0 +1,35 @@
+let h1 = document.querySelector("h1"); // seleccinnamos el elemento y to guordamos en ta variable h1
+h1.innerText = 'Productos'; // cambiamos el texto interno del h1 a "productos"
+
+
+let array = [];
+
+function cards() {
+
+    for (let i = 1; i < 10; i++) {
+        array.push(`
+            <div class="card">
+                <img src="https://66d9ee6caa07a954166f10ed--gregarious-melba-cacdba.netlify.app/${i}.jpg">
+                    <div>
+                        <h4> card ${i} </h4>
+                        <p>Esta es la card número ${i}.</p>
+                    </div>
+            </div>
+    `);
+
+    }
+    
+    // if (i == 1) {
+    //     title = "BMW M5"
+    //     price = "$500.000"
+    // } else if (i == 2) {
+    //     title = "Gaston Martin"
+    //     price = "$800.000"
+    // } else if (i == 3) {
+    //     title = "Mercedes AMG"
+    //     price = "$900.000"
+    // }
+
+    document.querySelector("section").innerHTML = array.join().replaceAll(",", "");
+}
+cards();
